b8a69078b885afda437cb52b36dd3e806d1c15b8
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-12-07 13:41:03 +0900

    어드민 레이아웃 헤더 숨김

--- components/layout/conditional-header.tsx
+++ components/layout/conditional-header.tsx
@@ -5,17 +5,16 @@ import { Header } from './header'
 
 export function ConditionalHeader() {
   const pathname = usePathname()
-  
-  // Hide header on settings pages
-  const hideOnPaths = ['/settings', '/w/']
-  console.log(hideOnPaths, pathname)
-  const shouldHideHeader = hideOnPaths.some(path => 
+
+  // Hide header on settings, admin, and wishlist sharing pages
+  const hideOnPaths = ['/settings', '/admin', '/w/']
+  const shouldHideHeader = hideOnPaths.some(path =>
     pathname.includes(path)
   )
-  
+
   if (shouldHideHeader) {
     return null
   }
-  
+
   return <Header />
 }
